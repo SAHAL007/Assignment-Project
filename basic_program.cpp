50a691840c30822888b2cd6a1ead74028afaea11
Sahal Chowdhury <sahal.ahamad@brainstation-23.com>
2024-04-25 10:35:32 +0600

    Added Fibonacci Sequence

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,2 +1,3 @@
 #include <stdio.h>\nint main() { printf("Hello World\\n"); return 0; }
 int factorial(int n) { return (n < 2) ? 1 : n * factorial(n - 1); }
+int fibonacci(int n) { return (n <= 1) ? n : fibonacci(n-1) + fibonacci(n-2); }
